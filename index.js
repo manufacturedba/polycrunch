6d36ba15320b0bfcf1f753a7251f1afe149f43a7
Roberto Rodriguez <manufacturedba@gmail.com>
2017-06-04 20:11:14 -0700

    refactor: Switch to Express because damnit I cannot use HTTP.incomingMessage

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const restify = require('restify');
-const server = restify.createServer();
+const express = require('express');
+const server = express();
+
+const PORT = process.env.PORT || 8000;
 
 function serveIndex(req, res, done){
   fs.readFile('dist/index.html', 'utf8', function(err, html){
@@ -14,9 +16,8 @@ function serveIndex(req, res, done){
 }
 
 function forceToSSL(req, res, done) {
-  console.log(req.connection.domain);
-
-  if (!req.socket.localPort) {
+  
+  if (!req.secure) {
     return res.redirect(301, 'https://' + path.join(req.headers.host + req.url), done);
   }
 
@@ -37,11 +38,8 @@ server.get('/resume.pdf', function(req, res, done) {
   })
 });
 
-server.get(/.*/, restify.serveStatic({
-  directory: __dirname + '/dist',
-  default: 'index.html'
-}));
+server.get('/', express.static('dist'));
 
-server.listen(process.env.PORT || 8000, function(){
-  console.log('Listening at ' + server.url);
+server.listen(PORT, function(){
+  console.log('Listening at ' + PORT);
 });
