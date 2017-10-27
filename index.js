923a69144dcf8eaba206fb3284006ea4da29ec75
Roberto Rodriguez <manufacturedba@gmail.com>
2017-10-27 01:05:24 -0700

    feat: POST/GET for all the fun

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const http = require('http');
 const express = require('express');
 const Primus = require('primus');
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+
+const DEAD_SECRET = process.env.DEAD_SECRET || Math.random();
+const MONGODB_URI = process.env.MONGODB_URI;
+const mongoUri = MONGODB_URI || 'mongodb://localhost:27017/polycrunch';
+
+
+const deferredClient = new Promise((resolve, reject) => {
+  return MongoClient.connect(mongoUri, function(err, db) {
+    if (err) {
+      return reject(err);
+    }
+
+    return resolve(db);
+  });
+});
 
 const app = express();
 const server = http.createServer(app);
@@ -33,7 +49,6 @@ function loadStaticAppRoutes() {
   APP_ROUTES.forEach(route => app.use(path.join(ROOT, route), express.static(STATIC_FOLDER)));
 }
 
-loadStaticAppRoutes();
 app.post('/update-temperature', function(req, res, done) {
 
   if (req.body.token === TEMPERATURE_TOKEN) {
@@ -54,6 +69,71 @@ app.post('/update-temperature', function(req, res, done) {
 
   done();
 });
+
+function crowTongue(db) {
+  return new Promise((resolve, reject) => {
+    const collection = db.collection('crypt');
+    return collection.find({}, { _id: 0 }).toArray((err, docs) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(docs);
+    })
+  });
+}
+
+function mixBoneBits(db, { link, name, recording }) {
+  return new Promise((resolve, reject) => {
+    const collection = db.collection('crypt');
+    return collection.insertOne({
+        source: {
+          link,
+          name
+        },
+        recording
+      }, (err, docs) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(docs);
+    });
+  });
+}
+
+app.post('/add-grave-dust', function(req, res, done) {
+
+  if (req.body.token !== DEAD_SECRET) {
+    return res.status(500).send('A curse on your family!!');
+  }
+
+  const { link, name, recording } = req.body.payload;
+
+  deferredClient
+  .then(db => mixBoneBits(db, {
+    link,
+    name,
+    recording
+  }))
+  .then(() => res.status(200).send('The dead shall rise'))
+  .catch(err => {
+    console.error(err);
+    res.status(500).send('Oopsies, I broke!');
+  });
+});
+
+app.get('/witches-brew', function(req, res, done) {
+  deferredClient
+  .then(db => crowTongue(db))
+  .then(catFaces => res.json(catFaces))
+  .catch(err => {
+    console.error(err);
+    res.status(500).send('Oopsies, I broke!');
+  });
+});
+
+loadStaticAppRoutes();
 
 server.listen(PORT, function(){
   console.log('Listening at ' + PORT);
